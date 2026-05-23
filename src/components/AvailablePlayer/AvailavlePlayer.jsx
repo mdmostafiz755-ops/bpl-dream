16b254f0c4f8934ec6ab4a85ddea1bb4427fde43
mdmostafiz755-ops <mdmostafiz755@gmail.com>
2026-05-23 21:20:58 +0600

    fixed alignment

--- src/components/AvailablePlayer/AvailavlePlayer.jsx
+++ src/components/AvailablePlayer/AvailavlePlayer.jsx
@@ -4,17 +4,21 @@ const AvailavlePlayer = ({ data }) => {
     console.log(data);
     return (
         <div>
-            <div className="card bg-base-100 w-96 shadow-sm">
+            <div className="card bg-base-100 w-96 shadow-md">
                 <figure>
                     <img
                         src={data.playerImage}
                         alt="Shoes" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">{data.playerName}</h2>
+                    <h2 className="card-title">
+                        {data.playerName}
+                        <div className="badge badge-secondary">{data.playerCountry}</div>
+                    </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <div className="badge badge-outline">Fashion</div>
+                        <div className="badge badge-outline">Products</div>
                     </div>
                 </div>
             </div>
